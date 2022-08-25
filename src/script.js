9b170888f09989bcc66adf10a8a9a6afb6186dc9
aya-0p <4587game.aya@gmail.com>
2022-08-25 20:58:08 +0900

    add

---
+++ src/script.js
@@ -0,0 +1,30 @@
+"use strict";
+//canvas取得
+const canvas = document.getElementById("canvas");
+if (!canvas || !(canvas instanceof HTMLCanvasElement))
+    throw new Error();
+const ctx = canvas.getContext("2d");
+if (!ctx)
+    throw new Error();
+//すべてのアイテムをロード
+let imageLoadStatus = 0;
+const imageItems = (() => {
+    const imageItemsOriginal = [];
+    const imageItemsMap = new Map();
+    imageItemsOriginal.forEach(item => {
+        const img = new Image();
+        img.src = item.path;
+        imageItemsMap.set(item.name, {
+            width: item.width,
+            height: item.height,
+            center: item.center,
+            img: img,
+            name: item.name,
+            category: item.category
+        });
+        img.addEventListener('load', () => {
+            imageLoadStatus++;
+        }, false);
+    });
+    return imageItemsMap;
+})();
---
+++ src/script.ts
@@ -0,0 +1,51 @@
+//canvas取得
+const canvas = document.getElementById("canvas")
+if (!canvas || !(canvas instanceof HTMLCanvasElement)) throw new Error()
+const ctx = canvas.getContext("2d")
+if (!ctx) throw new Error()
+//すべてのアイテムをロード
+let imageLoadStatus: number = 0
+const imageItems: Map<string, Items> = (() => {
+  const imageItemsOriginal: Array<TItems> = [
+
+  ]
+  const imageItemsMap: Map<string, Items> = new Map()
+  imageItemsOriginal.forEach(item => {
+    const img = new Image()
+    img.src = item.path
+    imageItemsMap.set(item.name, {
+      width: item.width,
+      height: item.height,
+      center: item.center,
+      img: img,
+      name: item.name,
+      category: item.category
+    })
+    img.addEventListener('load', () => {
+      imageLoadStatus++
+    }, false);
+  })
+  return imageItemsMap
+})()
+
+//interface
+interface Point {
+  x: number;
+  y: number;
+}
+interface TItems {
+  width: number;
+  height: number;
+  center: Point;
+  path: string;
+  name: string;
+  category: number;
+}
+interface Items {
+  width: number;
+  height: number;
+  center: Point;
+  img: HTMLImageElement;
+  name: string;
+  category: number;
+}
